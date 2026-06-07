a3e3d595a3e15c3e583b20d42798a1db6f1beaca
Takanori Ugai <ugai.takanori@gmail.com>
2026-06-07 13:28:27 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,7 +76,7 @@ dependencies {
     implementation("dev.langchain4j:langchain4j-open-ai-official:1.16.1-beta26")
     implementation("dev.langchain4j:langchain4j-google-ai-gemini:1.15.0")
     implementation("dev.langchain4j:langchain4j-ollama:1.15.0")
-    implementation("dev.langchain4j:langchain4j-community-neo4j:1.16.1-beta26")
+    implementation("dev.langchain4j:langchain4j-community-neo4j:1.15.0-beta25")
 
     implementation("com.auth0:java-jwt:4.5.1")
     // JTokkit
