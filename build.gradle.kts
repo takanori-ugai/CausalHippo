e6c6078597c954a404381087f5933fda5294239f
Takanori Ugai <ugai@fujitsu.com>
2026-08-17 16:20:42 +0900

    clean shadowshar warnings

--- build.gradle.kts
+++ build.gradle.kts
@@ -127,6 +127,9 @@ tasks {
 
     shadowJar {
         isZip64 = true
+        filesMatching("META-INF/*.kotlin_module") {
+            duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        }
     }
 }
 
